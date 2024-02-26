76434efe6a51e503a31372e1f29d6ab9ad5d8e44
M. Zasada <mzasada93@gmail.com>
2024-02-26 20:50:25 +0100

    add tailwind, prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  useTabs: true,
+  singleQuote: false,
+  semi: true,
+  printWidth: 120,
+};
